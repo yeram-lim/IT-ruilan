5a7a0c2da9ae49dedb9ca57ef1ff7adc8c00da79
yeram-lim <yeramyeye@gmail.com>
2022-04-01 20:22:56 +0900

    github action

---
+++ docs/deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build commands
+yarn build
+
+# navigate into the build output directory
+cd .vuepress/dist
+
+git init
+git add -A
+git commit -m "deploy with vuepress script"
+git push -f https://github.com/yeram-lim/IT-ruilan.git master:gh-pages
+
+cd -
+ 
