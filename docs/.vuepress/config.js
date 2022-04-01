5e7abffb418a4a4fd6e1f1fe1bcb11ffa4ae675a
yeram-lim <yeramyeye@gmail.com>
2022-04-01 21:10:26 +0900

    add public folder

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -19,7 +19,17 @@ module.exports = {
   head: [
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ["link", { rel: "shortcut icon", href: "/favicon.ico" }],
+    ["link", { rel: "manifest", href: "/manifest.json" }],
+    [
+      "link",
+      {
+        rel: "apple-touch-icon",
+        sizes: "192x192",
+        href: "images/maskable_icon_x192.png",
+      },
+    ],
   ],
 
   /**
@@ -69,5 +79,15 @@ module.exports = {
   plugins: [
     '@vuepress/plugin-back-to-top',
     '@vuepress/plugin-medium-zoom',
+    [
+      "@vuepress/pwa",
+      {
+        serviceWorker: true,
+        updatePopup: {
+          message: "새 컨텐츠가 등록되었습니다. 새로고침 버튼을 눌러주세요😀",
+          buttonText: "새로고침",
+        },
+      },
+    ],
   ]
 }
